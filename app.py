50c8d60ce5f995e4a890f9ffded0af2e0867ec7b
wangbo <wananoner@gmail.com>
2020-04-18 22:28:53 +0800

    0

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
-from flask import Flask, flash, render_template, request, abort
+from flask import Flask, flash, render_template, request, abort, url_for
 
 app = Flask(__name__)
-app.secret_key = '123'
-
 
 @app.route('/')
-def home():
-    flash("Hello, my girl")
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
 
-
-@app.route('/login', methods=['POST'])
-def login():
-    form = request.form
-    username = form.get('username')
-    password = form.get('password')
-    if not username:
-        flash("请输入用户名")
-        return render_template("index.html")
-    if not password:
-        flash("请输入密码")
-        return render_template("index.html")
-    if username == 'wangbo' and password == '123456':
-        flash("登陆成功")
-        return render_template("index.html")
-    else:
-        flash("用户名或密码错误")
-        return render_template("index.html")
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html"), 404
-
+@app.route('/user/<name>')
+def user(name):
+    return f'Hello, {name}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
