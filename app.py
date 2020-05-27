8547b383cfbadb0d6c3cce7cf184ca9ca00489fc
wangbo <wananoner@gmail.com>
2020-05-27 22:26:42 +0800

    0

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, flash, render_template, request, abort, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/user/<name>')
 def user(name):
     return f'Hello, {name}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
